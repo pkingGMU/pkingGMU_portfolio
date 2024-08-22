5bc6f677c78b2236536a5821c537aef9ae79ccbd
Patrick King <pat.k195@gmail.com>
2024-08-21 20:37:16 -0400

    Added more pictures and added function in main.js to zoom in on images

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
+// Description: JavaScript functions for the website
+
 // JavaScript functions to handle button clicks
 function scrollToElement(element) {
     document.getElementById(element).scrollIntoView({behavior: "smooth"});
     console.log("Scrolling to " + element);
 }
 
+// JavaScript functions to handle image clicks
+function zoomImage(imgElement) {
+
+    const allImages = document.querySelectorAll('img');
+    const isZoomed = imgElement.classList.contains("fullsize");
+    
+    if (isZoomed) {
+        imgElement.classList.remove("fullsize");
+        allImages.forEach(img => img.classList.remove('disabled'))
+    } else {
+        
+        allImages.forEach(img => {
+            if (img !== imgElement) {
+                img.classList.add('disabled');
+            }
+        });
+
+        if (imgElement) {
+            imgElement.classList.toggle("fullsize");
+        } else {
+            console.error("Image not found: " + image);
+        }
+
+    }
+    
+}
